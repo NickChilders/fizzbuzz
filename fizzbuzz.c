56031b55ed0a034e3edd804b261daec8a83cd63e
nrchilders <74997040+nrchilders@users.noreply.github.com>
2021-11-15 17:02:30 -0500

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i;
+    for(i = 1; i <= 100; i++){
+        if(i % 15 == 0){
+            printf("FizzBuzz ");
+        }
+        else if(i % 5 == 0){
+            printf("Buzz ");
+        }
+        else if(i % 3 == 0){
+            printf("Fizz ");
+        }
+        else
+            printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
